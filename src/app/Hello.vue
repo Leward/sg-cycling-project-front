8da5eb666e0a99411f72cc881a518462d48f35c0
Paul-Julien Vauthier <pj82491@gmail.com>
2017-02-11 22:50:20 +0800

    Landing page

---
+++ src/app/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="landing-page">
+    <header><h1 class="container">SG Cycling Project</h1></header>
+    <div class="container">
+      <p>
+        Welcome to the SG Cycling project which aims at providing a portal with tools for everyday cyclists in
+        Singapore.
+      </p>
+      <p>
+        At the moment we are working on providing a collaborative map for cyclists to find bicycle parking facilities in
+        Singapore.
+      </p>
+      <p>
+        <button class="btn btn-primary">Go to the bicycle parking map</button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<!--<div class="container landing-page">-->
+<!--<div class="jumbotron">-->
+<!--<h1>SG Cycling Project</h1>-->
+<!--<p>-->
+<!--Welcome to the SG Cycling project which aims at providing a portal with tools for everyday cyclists in-->
+<!--Singapore.-->
+<!--</p>-->
+<!--<p>-->
+<!--At the moment we are working on providing a collaborative map for cyclists to find bicycle parking facilities in-->
+<!--Singapore.-->
+<!--</p>-->
+<!--<p><a class="btn btn-primary btn-lg" href="#" role="button">To the parking map</a></p>-->
+<!--</div>-->
+<!--</div>-->
+
+<script>
+  export default {
+    name: 'Hello',
+    data() {
+      return {
+        hello: 'Hello World!'
+      };
+    }
+  };
+</script>
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Hello from './app/Hello.vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.scss';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      components: {
+        default: Hello
+      }
+    }
+  ]
+});
+
+export default new Vue({
+  el: '#root',
+  router,
+  render: h => h('router-view')
+});
